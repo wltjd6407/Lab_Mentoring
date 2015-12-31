7c8f38ab382117e9c91bdd34a29c5555f038fbaa
wltjd6407 <wltjd6407@naver.com>
2015-12-31 20:45:01 +0900

    151231 END-1

--- src/com/mypcr/Main.java
+++ src/com/mypcr/Main.java
@@ -1,9 +1,26 @@
 package com.mypcr;
 
+import com.mypcr.emulator.MyPCR;
+
 public class Main 
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("hello world3");
+		int test = 0;
+		
+		MyPCR mypcr = new MyPCR();
+		test = mypcr.add(1, 2);
+		
+		System.out.println(test);
+		
+		test = mypcr.sub(2, 1);
+		
+		System.out.println(test);
+		
+		test = mypcr.mul(1, 4);
+		
+		System.out.println(test);
+		
+		test = mypcr.div(2, 0);
 	}
 }
