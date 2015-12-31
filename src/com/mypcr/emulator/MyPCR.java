d6f8647322284f86f69b264e0241774f7c0ccdef
wltjd6407 <wltjd6407@naver.com>
2015-12-31 20:45:56 +0900

    151231 END-2

--- src/com/mypcr/emulator/MyPCR.java
+++ src/com/mypcr/emulator/MyPCR.java
@@ -1,5 +1,31 @@
 package com.mypcr.emulator;
 
 public class MyPCR {
-
+	public MyPCR()
+	{
+		
+	}
+	
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
+	
+	public int sub(int a, int b)
+	{
+		return a-b;
+	}
+	
+	public int div(int a, int b)
+	{
+		if( b == 0 )
+			return 0;
+		else
+			return a/b;
+	}
 }
