54995b583f6871c12309af8d9c23c141da9e13c5
wltjd6407 <wltjd6407@naver.com>
2016-02-04 20:49:23 +0900

    160204_Mentoring

--- src/com/mypcr/Main.java
+++ src/com/mypcr/Main.java
@@ -1,47 +1,73 @@
-
+/**
+ 1. startPCR(), stopPCR() 시 , PCR 시작!, PCR 종료!
+ 2. start시  mPrevTemp = 95.0, mTargetTemp = 50.0 으로 변경 
+    run() 함수 내에서 mPrevTemp 까지 온도 도달시키고, 그 후에 mTargetTemp까지 온도가 도달하도록 구현,
+    mTargetTemp 까지 온도가 도달할 경우, 자동으로 stop 되도록 구현
+ 3. startPCR(), stopPCR() 시, 현재 상태가 맞지 않은 경으 그냥 무시하도록 구현
+ 	ex) start 했는데 , state가 run이면 그냥 return 하도록 구현 stop시도
+ 4. private int mElpsedTime 선언 후, 0으로 초기화
+ 	run()함수에서 state가 run일 경우 1sec 마다 증가시킴
+ 	printStateus() 함수에서 String.format에다 elapsedTime : %s 추가
+ 	
+ 	mTemp += 0.01 로 변경
+ 	getElapsedTime()를 다음과 같이 구현하시오.
+ 	private String getElapsedTime();
+ **/
 package com.mypcr;
  
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
 
 import com.mypcr.emulator.MyPCR;
-import com.mypcr.emulator.Protocol;
 
 public class Main 
 {
 	public static void main(String[] args) 
 	{
-		BufferedReader in = null;
-		String pt = null;
-		MyPCR mypcr = new MyPCR();
-		ArrayList<String> strlist = new ArrayList<String>();
-		try {
-			in = new BufferedReader(new FileReader(new File("Protocol.txt")));
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		try {
-			String line = null;
-			while((line = in.readLine()) != null)
-			{
-				strlist.add(line);
-			}
-			if(strlist.get(0).equals("%PCR%") && strlist.get(strlist.size()-1).equals("%END%")){
-			
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		final MyPCR mypcr = new MyPCR();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		ArrayList<Protocol> protocol = mypcr.makeProtocolList(pt);
-		mypcr.showProtocolList(protocol);
+		mypcr.start();
+		while(true)
+		{
+			try 
+			{
+				String input = in.readLine();
+				if(input.equals("start")){
+					mypcr.startPCR();
+				}
+				else if(input.equals("stop")){
+					mypcr.stopPCR();
+				}
+				else if(input.equals("print")){
+					mypcr.printStatus();
+				}
+				else if(input.equals("monitor"))
+				{
+					mypcr.setMonitoring(true);
+					Thread t = new Thread(){
+						public void run(){
+							while(mypcr.isMonitoring()){
+								try{
+									Thread.sleep(10);
+								}catch(InterruptedException e){
+									e.printStackTrace();
+								}
+								mypcr.printStatus();
+							}
+						}
+					};
+					
+					t.start();
+					in.readLine();
+					mypcr.setMonitoring(false);
+				}
+			}catch (IOException e) {
+				e.printStackTrace();	
+			}
+				
+		}
 		
 	}
 }
--- src/com/mypcr/emulator/MyPCR.java
+++ src/com/mypcr/emulator/MyPCR.java
@@ -1,16 +1,56 @@
 package com.mypcr.emulator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class MyPCR 
+public class MyPCR extends Thread
 {
-	public MyPCR()
-	{
-		
+	private double mTemp;
+	private double mPrevTemp, mTargetTemp;
+	private int state;
+	
+	private static final int STATE_READY = 0x00;
+	private static final int STATE_RUN = 0x01;
+	
+	private static final double DEFAULT_TEMP = 25.1;
+	private boolean isMonitor = false;
+	
+	public MyPCR(){
+		mTemp = DEFAULT_TEMP;
+		mPrevTemp = DEFAULT_TEMP;
+		mTargetTemp = DEFAULT_TEMP;
+		state = STATE_READY;
 	}
+	
+	public void run(){
+		while(true){
+			try {
+				Thread.sleep(100);	
+			} catch (InterruptedException e){
+				e.printStackTrace();
+			}
+			
+			if(state == STATE_RUN){
+				mTemp += 0.5;
+			}
+			else if(state == STATE_READY){
+				mTemp = DEFAULT_TEMP;
+				mPrevTemp = DEFAULT_TEMP;
+				mTargetTemp = DEFAULT_TEMP;
+			}
+		}
+	}
+	
 	
 	public ArrayList<Protocol> makeProtocolList(String pcr)
 	{
+		if(pcr == null)
+			return null;
+		
 		ArrayList<Protocol> list = new ArrayList<Protocol>();
 		
 		try
@@ -53,4 +93,64 @@ public class MyPCR
 			System.out.println("잘못된 PCR파일 입니다.");
 		}
 	}
+	
+	public String loadProtocolFromFile(String path)
+	{
+		String result = "";
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new FileReader(new File(path)));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		try {
+			String line = null;
+			while((line = in.readLine()) != null)
+			{
+				result += line + "\n";
+			}
+			in.close();
+		} catch (IOException e1) {
+			// TODO: handle exception
+			e1.printStackTrace();
+			return null;
+		}
+		
+		return result;
+	}
+	
+	private String getStateString()
+	{
+		String curState = null;
+		if(state == STATE_READY)
+			curState = "준비중";
+		else if(state == STATE_RUN)
+			curState = "실행중";
+		
+		return curState;
+	}
+	
+	public void printStatus(){
+		System.out.println(String.format("상태 : %s , 온도 : %3.1f", getStateString(), mTemp));
+	}
+	
+	public void startPCR(){
+		state = STATE_RUN;
+	}
+	
+	public void stopPCR(){
+		state = STATE_READY;
+	}
+	
+	public void setMonitoring(boolean monitor){
+		isMonitor = monitor;
+	}
+	
+	public boolean isMonitoring(){
+		return isMonitor;
+	}
+	
+	
 }
